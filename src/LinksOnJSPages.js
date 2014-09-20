7b0ccdc318bfef95a85cfa0a6335fe9cff8cfdc1
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/LinksOnJSPages.js
+++ src/LinksOnJSPages.js
@@ -3,7 +3,6 @@
  * (Workaround for [[bugzilla:10410]] - see [[w:en:Wikipedia:Village pump (technical)/Archive 84#Script for wikilinks in CSS/JS pages]])
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/LinksOnJSPages.js]] ([[File:User:Helder.wiki/Tools/LinksOnJSPages.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, laxbreak:true, onevar: true */
 /*global jQuery, mediaWiki */
